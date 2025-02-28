17b61a5a98306ad35d4f5b9d893942bc16cde1ef
shashankyld <shashankyld@gmail.com>
2025-02-28 08:08:38 +0100

    Try to impliment delaunay with Pyslam so i can come back to this full slam implimentation later

--- slam_parameters.py
+++ slam_parameters.py
@@ -11,7 +11,7 @@ class SlamParameters:
 
     # STARTING AND ENDING FRAME
     kStartingFrameIdx = 0
-    kEndingFrameIdx = 100
+    kEndingFrameIdx = 50
 
     # Keyframe creation parameters
     MIN_DISTANCE_BETWEEN_KEYFRAMES = 1  # meters
